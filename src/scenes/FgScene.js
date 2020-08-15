c5fcd6765e604d147c06ca49b098763c50021a7a
Claudia <claudia.sinowato@gmail.com>
2020-08-15 15:56:41 -0400

    Added feature: inedible food falls from the sky; added lives tracking

--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -2,9 +2,13 @@ import "phaser";
 import Player from "../entity/Player";
 import Ground from "../entity/Ground";
 import Edibles from "../entity/Edibles";
+import Inedibles from "../entity/Inedibles";
 
 let score = 0;
 let scoreText;
+
+let lives = 3;
+let livesText;
 
 export default class FgScene extends Phaser.Scene {
   constructor() {
@@ -45,6 +49,12 @@ export default class FgScene extends Phaser.Scene {
       fill: "#000",
     });
 
+    //create livestext
+    livesText = this.add.text(16, 50, "lives: 3", {
+      fontSize: "30px",
+      fill: "#000",
+    });
+
     // Create ground group
     this.createGroups();
 
@@ -72,6 +82,21 @@ export default class FgScene extends Phaser.Scene {
     this.groundGroup.create(x, y, "ground").setScale(0.6).refreshBody();
   }
 
+  // Helper function to drop edible objects
+  createEdibles(x, y) {
+    // Make object dropped random
+    let edibles = ["apple", "banana", "carrot", "strawberry", "watermelon"];
+    let randomIdx = Math.floor(Math.random() * 5);
+    // Select random edible object
+    this.ediblesGroup.create(x, y, edibles[randomIdx]).setScale(0.09);
+  }
+
+  createInedibles(x, y) {
+    let inedibles = ["cherries", "garlic", "grapes", "onion", "redonion"];
+    let randomIdx = Math.floor(Math.random() * 5);
+    this.inediblesGroup.create(x, y, inedibles[randomIdx]).setScale(0.09);
+  }
+
   // Make all groups
   createGroups() {
     this.groundGroup = this.physics.add.staticGroup({ classType: Ground });
@@ -79,35 +104,32 @@ export default class FgScene extends Phaser.Scene {
 
     this.ediblesGroup = this.physics.add.group({ classType: Edibles });
     // Make x-coordinate for food drop random
-    let foodXaxis = Math.floor(Math.random() * 1000);
-    this.createEdibles(foodXaxis, 0);
-  }
+    let edibleXaxis = Math.floor(Math.random() * 1000);
+    this.createEdibles(edibleXaxis, 0);
 
-  // Helper function to drop edible objects
-  createEdibles(x, y) {
-    // Make object dropped random
-    let edibleObjects = [
-      "apple",
-      "banana",
-      "carrot",
-      "strawberry",
-      "watermelon",
-    ];
-    let randomIdx = Math.floor(Math.random() * 5);
-    // Select random edible object
-    this.ediblesGroup.create(x, y, edibleObjects[randomIdx]).setScale(0.09);
-    // this.ediblesGroup.body.setGravityY(100); //doesnt work for group
+    this.inediblesGroup = this.physics.add.group({ classType: Inedibles });
+    let inedibleXaxis = Math.floor(Math.random() * 1000);
+    this.createInedibles(inedibleXaxis, 0);
   }
 
   createCollisions() {
     this.physics.add.collider(this.player, this.groundGroup);
     // this.physics.add.collider(this.player, this.ediblesGroup);
 
-    // when player collides with edibles, use the collectEdibles function to remove (disable) fruit
+    // when player collides with edibles, use the collectEdibles function to remove (disable) food
     this.physics.add.overlap(
       this.player,
       this.ediblesGroup,
       this.collectEdibles,
+      null,
+      this
+    );
+
+    // when player collides with inedibles, use the collectIndibles function to remove (disable) food
+    this.physics.add.overlap(
+      this.player,
+      this.inediblesGroup,
+      this.collectInedibles,
       null,
       this
     );
@@ -135,10 +157,17 @@ export default class FgScene extends Phaser.Scene {
     });
   }
 
-  // when player collects edible fruit, make fruit disappear, and increase score
+  // when player collects edible food, make food disappear, and increase score
   collectEdibles(player, edible) {
     edible.disableBody(true, true);
     score += 10;
     scoreText.setText(`score: ${score}`);
+  }
+
+  // if player catches inedible food, make food disappear, decrease lives
+  collectInedibles(player, inedible) {
+    inedible.disableBody(true, true);
+    lives -= 1;
+    livesText.setText(`lives: ${lives}`);
   }
 }
